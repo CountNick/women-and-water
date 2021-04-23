757adac466a0b9c039b261488dc6971108d7a5bf
CountNick <meijer.nick1@gmail.com>
2021-04-23 11:13:21 +0200

    feat: working on back button

--- js/app.js
+++ js/app.js
@@ -38,30 +38,30 @@ const init = async(config) => {
   config.chapters.forEach((record, idx) => {
       /* These first two variables will hold each vignette, the chapter
       element will go in the container element */
-      var container = document.createElement('div');
-      var chapter = document.createElement('div');
+      const container = document.createElement('div');
+      const chapter = document.createElement('div');
       // Creates the title for the vignettes
       if (record.title) {
-          var title = document.createElement('h3');
+          const title = document.createElement('h3');
           title.innerText = record.title;
           chapter.appendChild(title);
       }
       // Creates the image for the vignette
       if (record.image) {
-          var image = new Image();
+          const image = new Image();
           image.src = record.image;
           chapter.appendChild(image);
       }
       // Creates the image credit for the vignette
       if (record.imageCredit) {
-          var imageCredit = document.createElement('p');
+          const imageCredit = document.createElement('p');
           imageCredit.classList.add('imageCredit');
-          imageCredit.innerHTML = 'Image credit: ' + record.imageCredit;
+          imageCredit.innerHTML = `Image credit: ${record.imageCredit}`;
           chapter.appendChild(imageCredit);
       }
       // Creates the description for the vignette
       if (record.description) {
-          var story = document.createElement('p');
+          const story = document.createElement('p');
           story.innerHTML = record.description;
           chapter.appendChild(story);
       }
@@ -90,37 +90,41 @@ const init = async(config) => {
       const fetchedOptions = fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${queryString}.json?country=nl&access_token=${mapboxgl.accessToken}`)
           .then(res => res.json())
           .then(data => data);
-
+      // handles the choice of an address
       chooseAddress(fetchedOptions)
   })
 
   const chooseAddress = async(data) => {
+      // removes the options container from DOM if it's already there
       if(document.querySelector('.options__container') !== null) {
           document.querySelector('.options__container').remove()
       }
+      // wait for the data
       const rawData = await data
+      // select only the features array
       const addressOptions = rawData.features
+      // create a element to store the address options in
       const optionsContainer = document.createElement('ul')
       optionsContainer.classList.add('options__container')
-      
+      // make a element for each adrress option
       addressOptions.forEach(option => {
-          console.log(option)
-
+          // make a list item for each option
           const listItem = document.createElement('li')
-
-          listItem.addEventListener('click', (e) => {
-              searchInput.parentElement.parentElement.remove()
-              searchInput.value = e.target.innerHTML;
-                  
-              plotHomeLocation(option.geometry.coordinates)
-              
-          })
-
+          // set the innerHTML of listItem to the placename of the object
           listItem.innerHTML = option.place_name
+          // append the listitem to the options container
           optionsContainer.appendChild(listItem)
-          
+          // add a click event to each 
+          listItem.addEventListener('click', (e) => {
+              // remove the whole search element from DOM
+              searchInput.parentElement.parentElement.remove()
+              // set the input value to the selected address
+              searchInput.value = e.target.innerHTML;
+              // plot the home location on the map
+              plotHomeLocation(option.geometry.coordinates)
+          })
       })
-
+      // inject the options container between search container and location button
       searchContainer.insertBefore(optionsContainer, locationButton) 
   }
 
@@ -137,21 +141,23 @@ const init = async(config) => {
       const storyElement = document.createElement('div');
       storyElement.className = "story__container"
 
-      storyElement.insertAdjacentHTML('afterbegin', `<button class="story__prev-btn">Previous</button> <button class="story__next-btn">Next</button> `)
+      storyElement.insertAdjacentHTML('afterbegin', `<button data-attribute="story__prev-btn" class="story__prev-btn">Previous</button> <button class="story__next-btn">Next</button> `)
 
       storyElement.appendChild(features)
-
+    
       document.querySelector('body').appendChild(storyElement)
+      map.getSource('mark').setData({type: 'Point', coordinates: coordinates})
+      // config.chapters[0](setLayerOpacity)
       //create the marker DOM element
-      const homeMarker = document.createElement('div');
-      homeMarker.className = 'home-marker';
-      homeMarker.style.backgroundColor = 'black';
-      homeMarker.style.width = "20px"
-      homeMarker.style.height = "20px"
-      // plot marker on user's home address
-      const marker = new mapboxgl.Marker(homeMarker)
-          .setLngLat(coordinates)
-          .addTo(map);
+      // const homeMarker = document.createElement('div');
+      // homeMarker.className = 'home-marker';
+      // homeMarker.style.backgroundColor = 'black';
+      // homeMarker.style.width = "20px"
+      // homeMarker.style.height = "20px"
+      // // plot marker on user's home address
+      // const marker = new mapboxgl.Marker(homeMarker)
+      //     .setLngLat(coordinates)
+      //     .addTo(map);
       // make query to get Dutch water data from tileset
       const query = `https://api.mapbox.com/v4/${config.tilesetConfig.tileset}/tilequery/${coordinates[0]},${coordinates[1]}.json?radius=${config.tilesetConfig.radius}&limit=${config.tilesetConfig.limit}&access_token=${mapboxgl.accessToken}`
       
@@ -167,34 +173,34 @@ const init = async(config) => {
 
               const randomWaterSource = longer[random]
               console.log(randomWaterSource)
-              
               // fill the tilequery data layer with a random object from the array
               const waterSource = map.getSource('tilequery')
               waterSource.setData(randomWaterSource);
 
               map.on('click', 'tilequery-points', (e) => console.log(randomWaterSource))
-              
-              
               // route source used: geoconverter.hsr.ch
               // fetch a route for the water spot
-              fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${waterSource._data.geometry.coordinates[0]},${waterSource._data.geometry.coordinates[1]};${marker._lngLat.lng},${marker._lngLat.lat}?geometries=geojson&access_token=${mapboxgl.accessToken}`)
+              fetch(`https://api.mapbox.com/directions/v5/mapbox/walking/${waterSource._data.geometry.coordinates[0]},${waterSource._data.geometry.coordinates[1]};${coordinates[0]},${coordinates[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`)
                 .then(res => res.json())
                 .then(data => {
                   console.log(data.routes[0])
                   // fil the line data layer with the route to the waterspot
                   map.getSource('line').setData(data.routes[0].geometry)
                 })
-          
       });
 
-      config.chapters[0].location.center = marker._lngLat
-      config.chapters[1].location.center = marker._lngLat
-      config.chapters[2].location.center = marker._lngLat
-      config.chapters[3].location.center = marker._lngLat
-      config.chapters[4].location.center = marker._lngLat
+      config.chapters[0].location.center = coordinates
+      config.chapters[1].location.center = coordinates
+      config.chapters[2].location.center = coordinates
+      config.chapters[3].location.center = coordinates
+      config.chapters[4].location.center = coordinates
+
+      console.log(config.chapters[0])
+
+      config.chapters[0].onChapterEnter.forEach(setLayerOpacity)
 
       map.flyTo({
-          center: marker._lngLat,
+          center: config.chapters[0].location.center,
           zoom: 17,
           bearing: 0,
           speed: 1,
@@ -204,17 +210,55 @@ const init = async(config) => {
       });
 
       // add click event to the next button
-      storyElement.children[1].addEventListener('click', (e) => {
+      storyElement.children[0].addEventListener('click', (e) => {
+        const children = [...e.target.nextElementSibling.nextElementSibling.children]
+        // updateStory(children, e, config, map, setLayerOpacity)
 
+        const currentChapterElement = children.find(child => child.classList.contains('active'))
+        const currentChapterObject = config.chapters.find(chap => chap.id === currentChapterElement.id)
+
+
+        const findCurrentObject = (obj) => obj.id === currentChapterElement.id
+        
+        let currInd = config.chapters.findIndex(findCurrentObject)
+        
+        const nextChapter = config.chapters[--currInd]
+        console.log(nextChapter)
+        const nextChapterElement = children.find(child => child.id === nextChapter.id)
+          
+        // nextChapterElement.classList.remove('eraseFromDom')
+
+        currentChapterElement.classList.remove('active')
+        // currentChapterElement.classList.add('eraseFromDom')
+
+
+              currentChapterElement.classList.add('eraseFromDom')
+              nextChapterElement.classList.remove('eraseFromDom')
+              const setActive = setTimeout(() => {
+                nextChapterElement.classList.add('active')
+              },1)
+              if(currentChapterObject.onChapterExit.length > 0) {
+                  currentChapterObject.onChapterExit.forEach(setLayerOpacity);
+              }
+
+
+        map.flyTo(nextChapter.location);
+
+        if (nextChapter.onChapterEnter.length > 0) {
+            nextChapter.onChapterEnter.forEach(setLayerOpacity);
+        } else {
+            console.log(e)
+        }
+      })
+
+      storyElement.children[1].addEventListener('click', (e) => {
+          
           // function could look like this: updateStory(event, operator, method)
           const children = [...e.target.nextElementSibling.children]
-          
+          // updateStory(children, e)
           const currentChapterElement = children.find(child => child.classList.contains('active'))
           const currentChapterObject = config.chapters.find(chap => chap.id === currentChapterElement.id)
 
-          // currentChapterElement.classList.remove('active')
-          
-          // const currentObject = config.chapters.find(chap => currentChapterElement.id === chap.id)
 
           const findCurrentObject = (obj) => obj.id === currentChapterElement.id
           
@@ -231,14 +275,15 @@ const init = async(config) => {
           currentChapterElement.classList.remove('active')
           // currentChapterElement.classList.add('eraseFromDom')
 
-          currentChapterElement.addEventListener('transitionend', (e) => {
               currentChapterElement.classList.add('eraseFromDom')
               nextChapterElement.classList.remove('eraseFromDom')
-              nextChapterElement.classList.add('active')
+              const setActive = setTimeout(() => {
+                nextChapterElement.classList.add('active')
+              },1)
               if(currentChapterObject.onChapterExit.length > 0) {
                   currentChapterObject.onChapterExit.forEach(setLayerOpacity);
               }
-          })
+
 
           map.flyTo(nextChapter.location);
 
@@ -257,12 +302,13 @@ const init = async(config) => {
     const layerType = map.getLayer(layer).type;
     return layerTypes[layerType];
   }
+
   const setLayerOpacity = (layer) => {
       const paintProps = getLayerPaintType(layer.layer);
       paintProps.forEach(function (prop) {
           map.setPaintProperty(layer.layer, prop, layer.opacity);
-  });
-}
+      });
+  }
 
 }
 
@@ -295,6 +341,39 @@ function addLayers(map) {
 
   map.on('load', (e) => {
     
+      map.addSource('mark', { // Add a new source to the map style: https://docs.mapbox.com/mapbox-gl-js/api/#map#addsource
+          type: "geojson",
+          data: {
+            "type": "FeatureCollection",
+            "features": []
+          }
+        });
+      
+      map.addLayer({
+        id: "home-marker",
+        type: "circle",
+        source: "mark",
+        paint: {
+          "circle-opacity": 0,
+          "circle-stroke-color": "yellow",
+          "circle-color": "black",
+          "circle-stroke-width": { // Set the stroke width of each circle: https://docs.mapbox.com/mapbox-gl-js/style-spec/#paint-circle-circle-stroke-width
+            stops: [
+              [0, 1],
+              [18, 3]
+            ],
+            base: 5
+          },
+          "circle-radius": { // Set the radius of each circle, as well as its size at each zoom level: https://docs.mapbox.com/mapbox-gl-js/style-spec/#paint-circle-circle-radius
+            stops: [
+              [12, 5],
+              [22, 180]
+            ],
+            base: 5
+          }
+        }
+      })
+
       map.addSource('radius', { // Add a new source to the map style: https://docs.mapbox.com/mapbox-gl-js/api/#map#addsource
           type: "geojson",
           data: {
@@ -330,6 +409,7 @@ function addLayers(map) {
           source: "tilequery", // Set the layer source
           paint: {
             "circle-opacity": 0,
+            "circle-stroke-opacity": 0,
             "circle-stroke-color": "white",
             "circle-stroke-width": { // Set the stroke width of each circle: https://docs.mapbox.com/mapbox-gl-js/style-spec/#paint-circle-circle-stroke-width
               stops: [
@@ -388,6 +468,46 @@ function addLayers(map) {
 
 }
 
-function updateStory(event, operator, method) {
-    
+function updateStory(elements, event, config, map, setLayerOpacity) {
+    console.log('update story event: ', event.target.className)
+
+    const currentChapterElement = elements.find(child => child.classList.contains('active'))
+    const currentChapterObject = config.chapters.find(chap => chap.id === currentChapterElement.id)
+
+    const findCurrentObject = (obj) => obj.id === currentChapterElement.id
+          
+    let currInd = config.chapters.findIndex(findCurrentObject)
+
+    let nextChapter
+
+    if(event.target.className === 'story__prev-btn') {
+      nextChapter = config.chapters[--currInd]
+    } else {
+      nextChapter = config.chapters[++currInd]
+    } 
+
+    console.log(nextChapter)
+    const nextChapterElement = elements.find(child => child.id === nextChapter.id)
+
+    currentChapterElement.classList.remove('active')
+          // currentChapterElement.classList.add('eraseFromDom')
+
+          currentChapterElement.addEventListener('transitionend', (e) => {
+              currentChapterElement.classList.add('eraseFromDom')
+              nextChapterElement.classList.remove('eraseFromDom')
+              const setActive = setTimeout(() => {
+                nextChapterElement.classList.add('active')
+              },1)
+              if(currentChapterObject.onChapterExit.length > 0) {
+                  currentChapterObject.onChapterExit.forEach(setLayerOpacity);
+              }
+          })
+
+          map.flyTo(nextChapter.location);
+
+          if (nextChapter.onChapterEnter.length > 0) {
+              nextChapter.onChapterEnter.forEach(setLayerOpacity);
+          } else {
+              // console.log(e)
+          }
 }
